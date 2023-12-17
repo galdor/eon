ffbb5585d79e7463596f174af1ccf0e9a899dc87
Nicolas Martyanoff <nicolas@n16f.net>
2023-12-17 19:55:47 +0100

    fix escript generation

--- src/eon_escript.erl
+++ src/eon_escript.erl
@@ -11,24 +11,46 @@ build(Escript = #{type := escript,
   OutputPath = filename:join([Root, Name]),
   BeamFiles = beam_files(Escript, Manifest),
   AppResourceFiles = application_resource_files(Escript, Manifest),
-  %% escript:create/2 only supports strings for filenames
-  Files = [eon_string:string(Path)
-           || Path <- AppResourceFiles ++ BeamFiles],
-  EbinPathArgs = [[" -pz ", Dir] || Dir <- file_directories(Files)],
-  MiscArgs =
+  Files = AppResourceFiles ++ BeamFiles,
+  ArchiveFiles = [archive_file(Name, Path) || Path <- Files],
+  Args =
     %% Enable Unicode support for printable character detection
-    [" -pc unicode"],
-  EmuArgs = io_lib:format("-escript main ~ts~ts ~ts",
-                          [MainModule, EbinPathArgs, MiscArgs]),
+    ["-pc unicode"],
+  EmuArgs = io_lib:format(" -escript main ~ts ~ts", [MainModule, Args]),
   Sections = [{shebang, "/usr/bin/env escript"},
               {emu_args, EmuArgs},
-              {archive, Files, []}],
+              {archive, ArchiveFiles, []}],
   case escript:create(OutputPath, Sections) of
     ok ->
       eon_fs:add_file_permissions(OutputPath, 8#0111),
       eon_fs:path(OutputPath);
     {error, Reason} ->
       throw({error, {escript_create, Reason}})
+  end.
+
+-spec archive_file(eon_fs:path(), unicode:chardata()) ->
+        {file:filename(), binary()}.
+archive_file(ArchiveName, Path) ->
+  %% From the documentation of the "code" module:
+  %%
+  %% At startup, both the top directory in the embedded archive as well as all
+  %% (second level) ebin directories in the embedded archive are added to the
+  %% code path.
+  %%
+  %% We use both "ebin/<filename>.beam" for simple projects and
+  %% "<app>/ebin/<filename>.beam" for projects with nested applications.
+  case file:read_file(Path) of
+    {ok, Data} ->
+      RelPath =
+        case lists:reverse(filename:split(Path)) of
+          [Filename, <<"ebin">>, AppName, <<"apps">> | _Rest] ->
+            filename:join([ArchiveName, AppName, <<"ebin">>, Filename]);
+          [Filename, <<"ebin">> | _Rest] ->
+            filename:join([ArchiveName, <<"ebin">>, Filename])
+        end,
+      {eon_fs:path_string(RelPath), Data};
+    {error, Reason} ->
+      throw({error, {read_file, Path, Reason}})
   end.
 
 -spec beam_files(eon_manifest:escript(), eon_manifest:manifest()) ->
@@ -41,9 +63,3 @@ beam_files(#{applications := Apps}, Manifest) ->
         [eon_fs:path()].
 application_resource_files(#{applications := Apps}, Manifest) ->
   [eon_app:resource_file_path(App, Manifest) || App <- Apps].
-
--spec file_directories([file:filename_all()]) -> [eon_fs:path()].
-file_directories(Files) ->
-  lists:map(fun eon_fs:path/1,
-            maps:keys(#{filename:dirname(Filename) => true
-                        || Filename <- Files})).
